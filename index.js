b1bd95e3007ce33124c33cda5d14a7e330170436
Duane David <duanedave17@gmail.com>
2022-08-01 08:13:51 -0700

    Set up all functionality

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
 const shelve = document.querySelector('#book-shelve');
 const form = document.querySelector('form');
-const btnDelete = document.querySelector('#delete');
 const btnAddBook = document.querySelector('#add-book');
 const bookInput = document.querySelector('#book-name');
 const authorInput = document.querySelector('#author-name');
 
 let bookShelve = [];
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  if (bookInput.value !== '' && authorInput.value !== '') {
-    createBook(bookInput.value, authorInput.value);
-
-    let newBook = document.createElement('div');
-    bookShelve.forEach((book) => {
-      newBook.innerHTML = `<div class="book">
-      <h3>${book.bookName}</h3>
-      <p>${book.author}</p>
-      <button class="delete" id="delete">Delete</button>
-      </div>`;
-    });
-
-    shelve.appendChild(newBook);
-
-    bookInput.value = '';
-    authorInput.value = '';
-  }
-});
 
 function createBook(book, author) {
   let newBook = {
@@ -34,11 +12,36 @@ function createBook(book, author) {
     author: author,
   };
   bookShelve.push(newBook);
+
+  let mybook = document.createElement('div');
+  bookShelve.forEach((book, idx) => {
+    mybook.innerHTML = `<div class="book">
+      <h3>${book.bookName}</h3>
+      <p>${book.author}</p>
+      <button class="delete" class="delete">Delete</button>
+      </div>`;
+
+    const deleteBtn = mybook.querySelector('.delete');
+    deleteBtn.addEventListener('click', () => {
+      bookShelve.splice(idx, 1);
+      mybook.remove();
+      storeShelve();
+      console.log(idx);
+    });
+  });
+
+  storeShelve();
+
+  shelve.appendChild(mybook);
 }
 
-//delete book
-function deleteBook(book) {
-  let index = bookShelve.indexOf(book);
-  bookShelve.splice(index, 1);
-  console.log(bookShelve);
-}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (bookInput.value !== '' && authorInput.value !== '') {
+    createBook(bookInput.value, authorInput.value);
+
+    bookInput.value = '';
+    authorInput.value = '';
+  }
+});
+

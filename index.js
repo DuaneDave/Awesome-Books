e5fc9d4249cef83d9c270db18921f6f152563c08
Duane David <duanedave17@gmail.com>
2022-08-01 03:58:27 -0700

    Wire up the form and add functionality

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+const shelve = document.querySelector('#book-shelve');
+const form = document.querySelector('form');
+const btnDelete = document.querySelector('#delete');
+const btnAddBook = document.querySelector('#add-book');
+const bookInput = document.querySelector('#book-name');
+const authorInput = document.querySelector('#author-name');
+
 let bookShelve = [];
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (bookInput.value !== '' && authorInput.value !== '') {
+    createBook(bookInput.value, authorInput.value);
+
+    let newBook = document.createElement('div');
+    bookShelve.forEach((book) => {
+      newBook.innerHTML = `<div class="book">
+      <h3>${book.bookName}</h3>
+      <p>${book.author}</p>
+      <button class="delete" id="delete">Delete</button>
+      </div>`;
+    });
+
+    shelve.appendChild(newBook);
+
+    bookInput.value = '';
+    authorInput.value = '';
+  }
+});
 
 function createBook(book, author) {
   let newBook = {
@@ -6,4 +34,11 @@ function createBook(book, author) {
     author: author,
   };
   bookShelve.push(newBook);
+}
+
+//delete book
+function deleteBook(book) {
+  let index = bookShelve.indexOf(book);
+  bookShelve.splice(index, 1);
+  console.log(bookShelve);
 }

cd71ddbd99d7502ee2133272ac07777d9eccea5b
Duane David <duanedave17@gmail.com>
2022-08-02 07:38:26 -0700

    Add Delete and add book functionality

---
+++ index.js
@@ -0,0 +1,96 @@
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+const forLs = [];
+
+/* eslint max-classes-per-file: ["error", 2] */
+
+class DisplayBook {
+  static addBook(newBook, index) {
+    const library = document.querySelector('#book-shelve');
+    if (!localStorage.getItem('books')) {
+      const noBook = document.createElement('p');
+      noBook.innerHTML = 'No books in library';
+      library.appendChild(noBook);
+    }
+    const container = document.createElement('div');
+    container.classList.add('book');
+    container.classList.add('flex');
+    container.innerHTML = `
+    <div class="book-details">
+     <h3>${newBook.title}</h3>
+     <p>${newBook.author}</p>
+    </div>
+     <button class="delete" data-remove=${index}>Delete</button>
+     `;
+    library.appendChild(container);
+
+    forLs.push(newBook);
+  }
+
+  static setLs() {
+    localStorage.setItem('books', JSON.stringify(forLs));
+  }
+
+  static getLs() {
+    if (localStorage.getItem('books')) {
+      const books = JSON.parse(localStorage.getItem('books'));
+      books.forEach((book, index) => {
+        const newBook = new Book(book.title, book.author);
+        DisplayBook.addBook(newBook, index);
+      });
+    } else {
+      localStorage.setItem('books', JSON.stringify(forLs));
+    }
+    const deleteBtn = document.querySelectorAll('.delete');
+    deleteBtn.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const index = e.target.dataset.remove;
+        forLs.splice(index, 1);
+        DisplayBook.setLs();
+        e.target.parentElement.remove();
+      });
+    });
+  }
+
+  static deleteBook(index) {
+    forLs.splice(index, 1);
+    DisplayBook.setLs();
+  }
+}
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#book-name').value;
+  const author = document.querySelector('#author-name').value;
+
+  if (title !== '' && author !== '') {
+    const newBook = new Book(title, author);
+    DisplayBook.addBook(newBook);
+
+    DisplayBook.setLs(newBook);
+
+    document.querySelector('#book-name').value = '';
+    document.querySelector('#author-name').value = '';
+
+    document.querySelector('#book-name').focus();
+  }
+
+  const deleteBtn = document.querySelectorAll('.delete');
+  deleteBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const index = e.target.dataset.remove;
+      forLs.splice(index, 1);
+      DisplayBook.setLs();
+      e.target.parentElement.remove();
+    });
+  });
+});
+
+DisplayBook.getLs();
